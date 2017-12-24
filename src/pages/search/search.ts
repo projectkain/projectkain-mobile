8c3b2b679b7c2ccf5881036e0002fe3b112e4271
Rain Tomista <rstomista@up.edu.ph>
2017-12-25 03:47:03 +0800

    Focus searchbar on page enter

--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { RestaurantProvider } from '../../providers/restaurant/restaurant';
@@ -15,6 +15,8 @@ export class SearchPage {
   currentItems: any = [];
   private restaurants: Observable<Restaurant[]>;
   private searchTerm: string = null;
+  @ViewChild('searchbar') searchbar;
+
   constructor(
     private navCtrl: NavController,
     private navParams: NavParams,
@@ -26,11 +28,15 @@ export class SearchPage {
     // this.restaurants = this.restaurantProvider.getRestaurants();
   }
 
+  ionViewDidEnter() {
+    this.searchbar.setFocus();
+  }
+
   search(event) {
     this.restaurants = this.restaurantProvider.getRestaurants();
     this.searchTerm = event.target.value;
     console.log(this.searchTerm);
-    if(this.searchTerm && this.searchTerm.trim() != '') {
+    if (this.searchTerm && this.searchTerm.trim() != '') {
       this.restaurants = this.restaurants.map(items => {
         return items.filter(e => {
           return e.name.trim().toLowerCase().includes(this.searchTerm.trim().toLowerCase());
